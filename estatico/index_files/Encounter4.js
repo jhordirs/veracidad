aac19b36390164aee809644184016117a608a2dd
jhordi <51026386+jhordirs@users.noreply.github.com>
2024-07-31 22:28:27 -0500

    Add files via upload

---
+++ estatico/index_files/Encounter4.js
@@ -0,0 +1,250 @@
+const Common = {
+    GetAllowedComponents: function (area, initialCount) {
+        const selected = $(`.se-btn-group[data-area="${area}"] button.btn-light`);
+
+        let components = {
+            C: initialCount,
+            S: initialCount,
+            T: initialCount,
+
+            Allow: function (components) {
+                let myComponents = {
+                    C: (components.match(/C/g) || []).length,
+                    S: (components.match(/S/g) || []).length,
+                    T: (components.match(/T/g) || []).length
+                };
+
+                for (const counter in myComponents) {
+                    if (this[counter] < myComponents[counter]) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        };
+
+        selected.each(function () {
+            for (let component of $(this).data('components')) {
+                components[component]--;
+            }
+        });
+
+        return components;
+    }
+}
+
+const Encounter4 = {
+    SequenceDissection: function () {
+        const buttons = $(`.se-btn-group button.btn-light`);
+        if (buttons.length < 6) return null;
+
+        const state = {
+            left: {
+                inside: null,
+                outside: null
+            },
+
+            middle: {
+                inside: null,
+                outside: null
+            },
+
+            right: {
+                inside: null,
+                outside: null
+            }
+        };
+
+        buttons.each(function () {
+            const button = $(this);
+            const area = button.closest('.se-btn-group').data('area');
+
+            state[button.closest('.card-body').data('position')][area]
+                = button.data('components');
+        });
+
+        for (const position in state) {
+            state[position].toAdd = function () {
+                let required = 'CST'.replace(this.inside, '');
+
+                for (let component of this.outside) {
+                    required = required.replace(component, '');
+                }
+
+                return required;
+            };
+
+            state[position].toRemove = function () {
+                if (this.outside.includes(this.inside)) {
+                    return this.inside;
+                }
+
+                for (let component of this.outside) {
+                    if (component != this.outside[0]) {
+                        return null;
+                    }
+                }
+
+                return this.outside[0];
+            };
+
+            state[position].swap = function (toRemove, toAdd) {
+                this.outside = this.outside.replace(toRemove, toAdd);
+            };
+        }
+
+        const steps = [];
+
+        while (true) {
+            const sequence = Encounter4.Evaluate(state);
+            if (sequence == null) break;
+
+            steps.push({
+                type: 'instruction',
+                text: `${Localization.dictionary.dissect} ${this._toIcon(sequence.componentA)} 
+                    ${Localization.dictionary.from} ${Localization.dictionary[sequence.positionA]}.`
+            });
+
+            steps.push({
+                type: 'instruction',
+                text: `${Localization.dictionary.dissect} ${this._toIcon(sequence.componentB)} 
+                    ${Localization.dictionary.from} ${Localization.dictionary[sequence.positionB]}.`
+            });
+
+            state[sequence.positionA].swap(sequence.componentA, sequence.componentB);
+            state[sequence.positionB].swap(sequence.componentB, sequence.componentA);
+
+            steps.push({
+                type: 'info',
+                text: `${Localization.dictionary.left}: ${this._toIcon(state.left.outside)}, 
+                    ${Localization.dictionary.middle}: ${this._toIcon(state.middle.outside)}, 
+                    ${Localization.dictionary.right}: ${this._toIcon(state.right.outside)}`
+            });
+        }
+
+        return steps;
+    },
+
+    Evaluate: function (state) {
+        var sequence = {
+            positionA: null,
+            componentA: null,
+
+            positionB: null,
+            componentB: null
+        };
+
+        for (const position in state) {
+            sequence.componentA = state[position].toRemove();
+
+            if (sequence.componentA != null) {
+                sequence.positionA = position;
+                break;
+            }
+        }
+
+        if (sequence.componentA === null) {
+            return null; // Done!
+        }
+
+        for (const position in state) {
+            if (state[position].toRemove() !== state[sequence.positionA].inside
+                    && state[position].toAdd().includes(sequence.componentA)) {
+                sequence.positionB = position;
+                sequence.componentB = state[position].toRemove();
+            }
+        }
+
+        // Force a necessary swap.
+        if (sequence.positionB === null) {
+            for (const position in state) {
+                if (state[position].toAdd().includes(sequence.componentA)) {
+                    sequence.positionB = position;
+                    sequence.componentB = state[position].toRemove();
+                }
+            }
+        }
+
+        return sequence;
+    },
+
+    _toIcon: function (component) {
+        switch (component) {
+            case 'C': return `<i class="symbol-sm circle"></i> (${Localization.dictionary.circle})`;
+            case 'S': return `<i class="symbol-sm square"></i> (${Localization.dictionary.square})`;
+            case 'T': return `<i class="symbol-sm triangle"></i> (${Localization.dictionary.triangle})`;
+
+            case 'CC':
+                return '<i class="symbol-sm sphere"></i>';
+            case 'SS':
+                return '<i class="symbol-sm cube"></i>';
+            case 'TT':
+                return '<i class="symbol-sm pyramid"></i>';
+            case 'CS': case 'SC':
+                return '<i class="symbol-sm cylinder"></i>';
+            case 'ST': case 'TS':
+                return '<i class="symbol-sm prism"></i>';
+            case 'TC': case 'CT':
+                return '<i class="symbol-sm cone"></i>';
+        }
+    }
+}
+
+$(function () {
+    $('.btn-reset').on('click', function () {
+        $('.se-btn-group button').each(function () {
+            const button = $(this);
+
+            button.removeClass('btn-light');
+            button.addClass('btn-dark');
+            button.attr('disabled', false);
+
+            $('#steps').empty();
+            $('#stepsContainer').hide();
+
+            window.scrollTo({ top: 0 });
+        });
+    });
+
+    $('.se-btn-group button').on('click', function () {
+        const button = $(this);
+        const area = button.closest('.se-btn-group').data('area');
+
+        // Block the ability to select buttons with invalid components.
+        let components = Common.GetAllowedComponents(area, (area === 'inside' ? 1 : 2));
+
+        $(`.se-btn-group[data-area="${area}"] button:not(.btn-light)`).each(function () {
+            $(this).attr('disabled', !components.Allow($(this).data('components')));
+        });
+
+        // Enable the reset button.
+        $('.btn-reset').attr('disabled', false);
+
+        // Determine sequence for dissection.
+        const steps = Encounter4.SequenceDissection();
+
+        // Print steps for dissection.
+        if (steps != null) {
+            $('#steps').empty();
+
+            for (let i = 0, step = 1; i < steps.length; i++) {
+                switch (steps[i].type) {
+                    case 'instruction':
+                        $('#steps').append(`<li>${step++}. ${steps[i].text}</li>`);
+                        break;
+
+                    case 'info':
+                        $('#steps').append(`<li>${steps[i].text}</li>`);
+                        if (i !== steps.length - 1) $('#steps').append('<hr />');
+                        break;
+                }
+            }
+
+            $('#stepsContainer').show();
+        }
+        else {
+            $('#stepsContainer').hide();
+        }
+    });
+});
---
+++ rotatorio/index_files/site.js
@@ -0,0 +1,103 @@
+// https://www.w3schools.com/charsets/ref_html_utf8.asp
+
+const Localization = {
+    _dictionaries: {
+        'en': {
+            title: 'Ayudante de Veracidad - Reto Rotatorio (Geometria Diversa)',
+            created_by: 'Creado por',
+            updated_by: 'actualizado por',
+
+            // 4th Encounter
+            encounter_4: '4to Encuentro',
+            dissection_tool: 'Herramienta de Disecci&oacuten',
+            reset: 'Reiniciar',
+            left: 'Izquierda',
+            middle: 'Medio',
+            right: 'Derecha',
+            inside: 'Adentro',
+            outside: 'Afuera',
+            circle: 'C&iacuterculo',
+            square: 'Cuadrado',
+            triangle: 'Tri&aacutengulo',
+            sphere: 'Esfera',
+            cube: 'Cubo',
+            pyramid: 'Pir&aacutemide',
+            cylinder: 'Cilindro',
+            prism: 'Prisma',
+            cone: 'Cono',
+            steps_to_solve: 'Pasos para resolver',
+            dissect: 'Diseccionar',
+            from: 'de'
+        },
+
+        'es': {
+            title: 'Ayudante de Veracidad - Reto Rotatorio (Geometria Diversa)',
+            created_by: 'Creado por',
+            updated_by: 'actualizado por',
+
+            // 4th Encounter
+            encounter_4: '4to Encuentro',
+            dissection_tool: 'Herramienta de Disecci&oacuten',
+            reset: 'Reiniciar',
+            left: 'Izquierda',
+            middle: 'Medio',
+            right: 'Derecha',
+            inside: 'Adentro',
+            outside: 'Afuera',
+            circle: 'C&iacuterculo',
+            square: 'Cuadrado',
+            triangle: 'Tri&aacutengulo',
+            sphere: 'Esfera',
+            cube: 'Cubo',
+            pyramid: 'Pir&aacutemide',
+            cylinder: 'Cilindro',
+            prism: 'Prisma',
+            cone: 'Cono',
+            steps_to_solve: 'Pasos para resolver',
+            dissect: 'Diseccionar',
+            from: 'de'
+        }
+    },
+
+    dictionary: null,
+
+    Initialize: function () {
+        const lang = this._getLang();
+        const dictionary = this._dictionaries[lang];
+
+        $("[data-localize]").each(function () {
+            const language = $(this).data('localize');
+            $(this).html(dictionary[language]);
+        });
+
+        this.dictionary = dictionary;
+    },
+
+    _getLang: function () {
+        const supportedLanguages = ['en', 'es'];
+
+        for (let i = 0; i < navigator.languages.length; i++) {
+            if (supportedLanguages.indexOf(navigator.languages[i]) != -1) {
+                return navigator.languages[i];
+            }
+        }
+
+        return 'en';
+    }
+}
+
+$(function () {
+    Localization.Initialize();
+
+    $('.se-btn-group button').on('click', function () {
+        const button = $(this);
+        const group = button.closest('.se-btn-group');
+        const wasSelected = button.hasClass('btn-light');
+
+        group.find('button').removeClass('btn-light');
+        group.find('button').addClass('btn-dark');
+
+        button.toggleClass('btn-dark', wasSelected);
+        button.toggleClass('btn-light', !wasSelected);
+    });
+});
